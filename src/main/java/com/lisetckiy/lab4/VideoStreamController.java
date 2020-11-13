694f98fcade50670eee19e8bb5b969a1510e2752
Derananer <10ilay1998@gmail.com>
2020-11-13 19:54:53 +0400

    initial

---
+++ src/main/java/com/lisetckiy/lab4/VideoStreamController.java
@@ -0,0 +1,34 @@
+package com.lisetckiy.lab4;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+@RestController("/stream")
+public class VideoStreamController {
+
+
+    @GetMapping("/video")
+    public StreamingResponseBody stream(){
+        final InputStream videoFileStream = new FileInputStream(videoFile);
+        return (os) -> {
+            readAndWrite(videoFileStream, os);
+        };
+    }
+
+    private void readAndWrite(final InputStream is, OutputStream os) throws IOException {
+        byte[] data = new byte[2048];
+        int read = 0;
+        while ((read = is.read(data)) > 0) {
+            os.write(data, 0, read);
+        }
+        os.flush();
+    }
+
+}
